b6f51f960edab18f4feb7b88a0fbe88e9b92e95c
dan121ka <somoseldia27@gmail.com>
2026-09-01 21:36:28 -0400

    Update eventStore.ts

--- modulo-5-inventario/src/store/eventStore.ts
+++ modulo-5-inventario/src/store/eventStore.ts
@@ -1,1 +1,23 @@
+import { EventoStock } from '../types/events';
 
+export class EventStore {
+  private readonly historial: ReadonlyArray<EventoStock>;
+
+  constructor(historialInicial: ReadonlyArray<EventoStock> = []) {
+    this.historial = Object.freeze([...historialInicial]);
+  }
+
+  // Registra un evento de forma append-only sin mutar el historial anterior
+  public agregarEvento(nuevoEvento: EventoStock): EventStore {
+    const eventoCongelado = Object.freeze({ ...nuevoEvento });
+    return new EventStore([...this.historial, eventoCongelado]);
+  }
+
+  public obtenerEventosPorProducto(productoId: string): ReadonlyArray<EventoStock> {
+    return Object.freeze(this.historial.filter(e => e.productoId === productoId));
+  }
+
+  public obtenerHistorialCompleto(): ReadonlyArray<EventoStock> {
+    return this.historial;
+  }
+}
